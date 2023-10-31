9fabe979c08fdbcae0171b4620b6dc9284e95463
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-10-31 19:19:38 -0300

    feat: response and request interceptors

--- src/service/HttpRequestService.ts
+++ src/service/HttpRequestService.ts
@@ -5,6 +5,33 @@ import { S3Service } from "./S3Service";
 const url =
   process.env.REACT_APP_API_URL || "http://localhost:8080/api";
 
+axios.interceptors.request.use(
+    config => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers['Authorization'] = token;
+      }
+      return config;
+    },
+    error => {
+      Promise.reject(error);
+    }
+)
+
+axios.interceptors.response.use(
+    response => {
+      return response;
+    },
+    error => {
+      if (error.response.status === 401) {
+        localStorage.removeItem("token");
+        window.location.href = "/";
+      }
+      return error;
+    }
+)
+
+
 const httpRequestService = {
   signUp: async (data: Partial<SingUpData>) => {
     const res = await axios.post(`${url}/auth/signup`, data);
