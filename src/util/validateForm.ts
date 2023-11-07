c8ed23b91e420392dfbcbfd477b9926d0c72d90a
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-11-07 13:25:54 -0300

    feat: added i18n to yup validators

--- src/util/validateForm.ts
+++ src/util/validateForm.ts
@@ -1,21 +1,26 @@
 import * as yup from 'yup'
+import i18next from "i18next";
 
 export const signInValidate = yup.object().shape({
-    email: yup.string().email().required(),
-    password: yup.string().required()
+    email: yup.string().email(i18next.t('error.email-error')).required(i18next.t('error.required')),
+    password: yup.string().required(i18next.t('error.required'))
 })
 
 export const signUpValidate = yup.object().shape({
-    name: yup.string().required().max(35),
-    username: yup.string().required(),
-    email: yup.string().email().required(),
+    name: yup.string().required(i18next.t('error.required')).max(35, i18next.t('error.max-35')),
+    username: yup.string().required(i18next.t('error.required')),
+    email: yup.string().email(i18next.t('error.email-error')).required(i18next.t('error.required')),
     password: yup
         .string()
-        .required('Please Enter your password')
+        .required(i18next.t('error.required'))
         .matches(
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*\.])(?=.{8,})/,
-            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+            i18next.t('error.password-error')
         ),
-    confirmPassword: yup.string().required().oneOf([yup.ref('password')], 'Passwords must match')
+    confirmPassword: yup.string().required(i18next.t('error.required')).oneOf([yup.ref('password')], 'Passwords must match')
 })
 
+export const postValidate = yup.object().shape({
+    content: yup.string().required(i18next.t('error.required')).max(240, i18next.t('error.content-length')),
+    images: yup.array().max(4, i18next.t('error.max-images'))
+})
